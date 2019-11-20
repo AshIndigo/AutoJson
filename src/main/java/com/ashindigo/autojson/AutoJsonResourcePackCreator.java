76b7f6e56437cefbea2c4ac1a124d943a002ccbb
Draylar <samuelhimes@gmail.com>
2019-11-20 15:30:20 -0600

    make resource pack always enabled

--- src/main/java/com/ashindigo/autojson/AutoJsonResourcePackCreator.java
+++ src/main/java/com/ashindigo/autojson/AutoJsonResourcePackCreator.java
@@ -8,6 +8,6 @@ import java.util.Map;
 public class AutoJsonResourcePackCreator implements ResourcePackProvider {
     @Override
     public <T extends ResourcePackProfile> void register(Map<String, T> map, ResourcePackProfile.Factory<T> factory) {
-        map.put("autojson", ResourcePackProfile.of("autojson/autojson", false, AutoJsonResourcePack::new, factory, ResourcePackProfile.InsertionPosition.BOTTOM));
+        map.put("autojson", ResourcePackProfile.of("autojson/autojson", true, AutoJsonResourcePack::new, factory, ResourcePackProfile.InsertionPosition.BOTTOM));
     }
 }
