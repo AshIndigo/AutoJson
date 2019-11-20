bef1779e035c0cfc502128a224d34b05ebd3b6ef
Draylar <samuelhimes@gmail.com>
2019-11-20 14:50:38 -0600

    swap resource reload listener with resource pack manager registry

--- src/main/java/com/ashindigo/autojson/AutoJsonClient.java
+++ src/main/java/com/ashindigo/autojson/AutoJsonClient.java
@@ -1,25 +1,11 @@
 package com.ashindigo.autojson;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.MinecraftClient;
 
 public class AutoJsonClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
-            @Override
-            public Identifier getFabricId() {
-                return new Identifier("autojson", "resource_pack");
-            }
-
-            @Override
-            public void apply(ResourceManager manager) {
-                manager.addPack(new AutoJsonResourcePack());
-            }
-        });
+        MinecraftClient.getInstance().getResourcePackManager().registerProvider(new AutoJsonResourcePackCreator());
     }
 }
